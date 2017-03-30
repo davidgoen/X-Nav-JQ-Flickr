36825e9f407235915f3814137081bd7c64cfc87e
davidgoen <davidgoen@hotmail.com>
2017-03-30 16:27:53 +0200

    Modifi2

--- jsonf.js
+++ jsonf.js
@@ -1,34 +1,29 @@
-function getImgs(resp){
-    $("#images").html("");
+function obtImagenes(resp){
+    $("#imagenes").html("");
     var html = "<ul>"
     for (var i = 0; i < 5; i++){
         html += "<li><h4>" + resp.items[i].title + "</h4>"
         html += "<img src='" + resp.items[i].media.m + "'></li>"
     }
     html += "</ul>"
-    $("#images").append(html)
+    $("#imagenes").append(html)
 }
 
-
 $(document).ready(function(){
-
    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
-
-  $("#tag").click(function(){
+  $("#tagFuenla").click(function(){
     $.getJSON(flickerAPI, {
       tags: "fuenlabrada",
       tagmode: "any",
       format: "json",
-    }).done(getImgs)
+    }).done(obtImagenes)
   });
-
-  $("#othertag").click(function(){
-    var tag = $("#choosetag").val()
+  $("#otroTag").click(function(){
+    var tag = $("#tagElegido").val()
     $.getJSON(flickerAPI, {
       tags: tag,
       tagmode: "any",
       format: "json",
-    }).done(getImgs)
+    }).done(obtImagenes)
   });
-
 });
